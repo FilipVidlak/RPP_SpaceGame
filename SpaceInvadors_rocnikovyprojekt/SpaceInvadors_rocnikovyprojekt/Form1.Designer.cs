c472c7f8d5397134a08236a15b9f80f138213509
FilipVidlak <72192554+FilipVidlak@users.noreply.github.com>
2023-03-23 15:16:44 +0100

    Add files via upload

---
+++ SpaceInvadors_rocnikovyprojekt/SpaceInvadors_rocnikovyprojekt/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace SpaceInvadors_rocnikovyprojekt
+{
+    partial class Vesmir
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Vesmir));
+            this.Restart_button = new System.Windows.Forms.Button();
+            this.Exit_button = new System.Windows.Forms.Button();
+            this.Vesmirnalod = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Background_timer = new System.Windows.Forms.Timer(this.components);
+            this.Neprmun_timer = new System.Windows.Forms.Timer(this.components);
+            this.Neprlode_timer = new System.Windows.Forms.Timer(this.components);
+            this.Municehrace_timer = new System.Windows.Forms.Timer(this.components);
+            this.Pohybdoleva_timer = new System.Windows.Forms.Timer(this.components);
+            this.Pohybdoprava_timer = new System.Windows.Forms.Timer(this.components);
+            this.Pohybdolu_timer = new System.Windows.Forms.Timer(this.components);
+            this.Pohybnahoru_timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Vesmirnalod)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Restart_button
+            // 
+            this.Restart_button.Location = new System.Drawing.Point(302, 202);
+            this.Restart_button.Name = "Restart_button";
+            this.Restart_button.Size = new System.Drawing.Size(144, 74);
+            this.Restart_button.TabIndex = 0;
+            this.Restart_button.Text = "Restart";
+            this.Restart_button.UseVisualStyleBackColor = true;
+            this.Restart_button.Click += new System.EventHandler(this.Restart_button_Click);
+            // 
+            // Exit_button
+            // 
+            this.Exit_button.Location = new System.Drawing.Point(302, 318);
+            this.Exit_button.Name = "Exit_button";
+            this.Exit_button.Size = new System.Drawing.Size(144, 67);
+            this.Exit_button.TabIndex = 1;
+            this.Exit_button.Text = "Ukončit";
+            this.Exit_button.UseVisualStyleBackColor = true;
+            this.Exit_button.Click += new System.EventHandler(this.Exit_button_Click);
+            // 
+            // Vesmirnalod
+            // 
+            this.Vesmirnalod.BackColor = System.Drawing.Color.Transparent;
+            this.Vesmirnalod.Image = ((System.Drawing.Image)(resources.GetObject("Vesmirnalod.Image")));
+            this.Vesmirnalod.Location = new System.Drawing.Point(324, 477);
+            this.Vesmirnalod.Name = "Vesmirnalod";
+            this.Vesmirnalod.Size = new System.Drawing.Size(81, 73);
+            this.Vesmirnalod.TabIndex = 2;
+            this.Vesmirnalod.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(13, 9);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 30);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Úroveň:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(113, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 30);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "01";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(637, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 30);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Skóre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(730, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 30);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(211, 84);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(344, 60);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "KONEC HRY";
+            // 
+            // Background_timer
+            // 
+            this.Background_timer.Enabled = true;
+            this.Background_timer.Interval = 10;
+            this.Background_timer.Tick += new System.EventHandler(this.Background_timer_Tick);
+            // 
+            // Neprmun_timer
+            // 
+            this.Neprmun_timer.Enabled = true;
+            this.Neprmun_timer.Interval = 60;
+            this.Neprmun_timer.Tick += new System.EventHandler(this.Neprmun_timer_Tick);
+            // 
+            // Neprlode_timer
+            // 
+            this.Neprlode_timer.Enabled = true;
+            this.Neprlode_timer.Tick += new System.EventHandler(this.Neprlode_timer_Tick);
+            // 
+            // Municehrace_timer
+            // 
+            this.Municehrace_timer.Enabled = true;
+            this.Municehrace_timer.Interval = 25;
+            this.Municehrace_timer.Tick += new System.EventHandler(this.Municehrace_timer_Tick);
+            // 
+            // Pohybdoleva_timer
+            // 
+            this.Pohybdoleva_timer.Interval = 5;
+            this.Pohybdoleva_timer.Tick += new System.EventHandler(this.Pohybdoleva_timer_Tick);
+            // 
+            // Pohybdoprava_timer
+            // 
+            this.Pohybdoprava_timer.Interval = 5;
+            this.Pohybdoprava_timer.Tick += new System.EventHandler(this.Pohybdoprava_timer_Tick);
+            // 
+            // Pohybdolu_timer
+            // 
+            this.Pohybdolu_timer.Interval = 5;
+            this.Pohybdolu_timer.Tick += new System.EventHandler(this.Pohybdolu_timer_Tick);
+            // 
+            // Pohybnahoru_timer
+            // 
+            this.Pohybnahoru_timer.Interval = 5;
+            this.Pohybnahoru_timer.Tick += new System.EventHandler(this.Pohybnahoru_timer_Tick);
+            // 
+            // Vesmir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(782, 562);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Vesmirnalod);
+            this.Controls.Add(this.Exit_button);
+            this.Controls.Add(this.Restart_button);
+            this.MaximumSize = new System.Drawing.Size(800, 609);
+            this.MinimumSize = new System.Drawing.Size(800, 609);
+            this.Name = "Vesmir";
+            this.Text = "Space Invadors";
+            this.Load += new System.EventHandler(this.Vesmir_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Vesmir_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Vesmir_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Vesmirnalod)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Restart_button;
+        private System.Windows.Forms.Button Exit_button;
+        private System.Windows.Forms.PictureBox Vesmirnalod;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer Background_timer;
+        private System.Windows.Forms.Timer Neprmun_timer;
+        private System.Windows.Forms.Timer Neprlode_timer;
+        private System.Windows.Forms.Timer Municehrace_timer;
+        private System.Windows.Forms.Timer Pohybdoleva_timer;
+        private System.Windows.Forms.Timer Pohybdoprava_timer;
+        private System.Windows.Forms.Timer Pohybdolu_timer;
+        private System.Windows.Forms.Timer Pohybnahoru_timer;
+    }
+}
+
---
+++ SpaceInvadors_rocnikovyprojekt/SpaceInvadors_rocnikovyprojekt/Form1.cs
@@ -0,0 +1,463 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+
+namespace SpaceInvadors_rocnikovyprojekt
+{
+    public partial class Vesmir : Form
+    {
+        private SoundPlayer soundtrack;
+        private SoundPlayer exploze;
+        private PictureBox[] hvezdy;
+        private PictureBox[] neprlode;
+        private PictureBox[] neprmun;
+        private PictureBox[] municehrace;
+        private Random rnd;
+
+        private int rychlostpozadi;
+        private int rychlostneprmun;
+        private int rychlostneprlode;
+        private int rychlostvesmirnelodi;
+        private int rychlostmunhrace;
+        private int skore;
+        private int uroven;
+        private int obtiznost;
+        private int pocetzmacknuti;
+        private int pocetstisknutiklaves;
+
+        private bool pauza;
+        private bool konechry;
+
+        public Vesmir()
+        {
+            InitializeComponent();
+        }
+
+        private void Restart_button_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void Exit_button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Konechry(string text)
+        {
+            if (text == "KONEC HRY")
+            {
+                exploze.Play();
+                label5.Location = new Point(145, 50);
+                label5.Text = text.ToUpper();
+                label5.Visible = true;
+                konechry = true;
+                pauza = true;
+                Restart_button.Visible = true;
+                Exit_button.Visible = true;
+                StopTimers();
+            }
+            else
+            {
+                soundtrack.Stop();
+                label5.Location = new Point(145, 50);
+                label5.Text = text.ToUpper();
+                label5.Visible = true;
+                konechry = true;
+                pauza = true;
+                Restart_button.Visible = true;
+                Exit_button.Visible = true;
+                StopTimers();
+            }
+        }
+        private void Zvuky()
+        {
+            soundtrack = new SoundPlayer("../../sounds/soundtrack.wav");
+            exploze = new SoundPlayer("../../sounds/exploze.wav");
+            soundtrack.PlayLooping();
+        }
+        private void InicializacePictureBoxu()
+        {
+            neprlode = new PictureBox[10];
+            neprmun = new PictureBox[10];
+            municehrace = new PictureBox[3];
+            hvezdy = new PictureBox[8];
+        }
+        private void InicializacePozadi()
+        {
+            for (int i = 0; i < hvezdy.Length; i++)
+            {
+                hvezdy[i] = new PictureBox
+                {
+                    BorderStyle = BorderStyle.None
+                };
+                int sirka = rnd.Next(20, 580);
+                int vyska = rnd.Next(-10, 400);
+                hvezdy[i].Location = new Point(sirka, vyska);
+                if (i % 2 == 0)
+                {
+                    hvezdy[i].Size = new Size(3, 3);
+                    hvezdy[i].BackColor = Color.WhiteSmoke;
+                }
+                else
+                {
+                    hvezdy[i].Size = new Size(4, 4);
+                    hvezdy[i].BackColor = Color.DarkGray;
+                }
+                this.Controls.Add(hvezdy[i]);
+            }
+        }
+
+        private void InicializaceNepratel()
+        {
+            for (int i = 0; i < neprlode.Length; i++)
+            {
+                neprlode[i] = new PictureBox
+                {
+                    Size = new Size(54, 54),
+                    Visible = true,
+                    Location = new Point((i + 1) * 50, -50)
+                };
+                this.Controls.Add(neprlode[i]);
+            }
+            Image neprlode_1 = Image.FromFile(@"../../images/neprlod_1.png");
+            Image neprlode_2 = Image.FromFile(@"../../images/neprlod_2.png");
+            Image neprlode_3 = Image.FromFile(@"../../images/neprlod_3.png");
+            Image boss = Image.FromFile(@"../../images/boss.png");
+            neprlode[0].Image = boss;
+            neprlode[1].Image = neprlode_2;
+            neprlode[2].Image = neprlode_3;
+            neprlode[3].Image = neprlode_3;
+            neprlode[4].Image = neprlode_1;
+            neprlode[5].Image = neprlode_3;
+            neprlode[6].Image = neprlode_2;
+            neprlode[7].Image = neprlode_3;
+            neprlode[8].Image = neprlode_2;
+            neprlode[9].Image = boss;
+        }
+
+        private void InicializaceMunNepratel()
+        {
+            for (int i = 0; i < neprmun.Length; i++)
+            {
+                neprmun[i] = new PictureBox
+                {
+                    Size = new Size(2, 20),
+                    BackColor = Color.Red,
+                    Visible = true
+                };
+                int random = rnd.Next(0, 10);
+                neprmun[i].Location = new Point(neprlode[random].Location.X, neprlode[random].Location.Y - 15);
+                this.Controls.Add(neprmun[i]);
+            }
+        }
+
+        private void InicializaceMunHrace()
+        {
+            for (int i = 0; i < municehrace.Length; i++)
+            {
+                municehrace[i] = new PictureBox
+                {
+                    Size = new Size(6, 6),
+                    BackColor = Color.Gold,
+                    Visible = true,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    BorderStyle = BorderStyle.None
+                };
+                this.Controls.Add(municehrace[i]);
+            }
+        }
+
+        private void Vesmir_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                rnd = new Random();
+
+                InicializacePictureBoxu();
+
+                InicializacePozadi();
+
+                InicializaceNepratel();
+
+                InicializaceMunNepratel();
+
+                InicializaceMunHrace();
+
+                Zvuky();
+
+                rychlostpozadi = 5;
+                rychlostneprmun = 4;
+                rychlostneprlode = 4;
+                rychlostvesmirnelodi = 6;
+                rychlostmunhrace = 15;
+                skore = 0;
+                uroven = 1;
+                obtiznost = 10;
+                pocetzmacknuti = 0;
+                pocetstisknutiklaves = 0;
+
+                label5.Visible = false;
+                Restart_button.Visible = false;
+                Exit_button.Visible = false;
+                pauza = false;
+                konechry = false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Nastala tato chyba: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Background_timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < hvezdy.Length / 2; i++)
+            {
+                hvezdy[i].Top += rychlostpozadi;
+                if (hvezdy[i].Top >= this.Height)
+                {
+                    hvezdy[i].Top = -hvezdy[i].Height;
+                }
+            }
+
+            for (int i = hvezdy.Length / 2; i < hvezdy.Length; i++)
+            {
+                hvezdy[i].Top += rychlostpozadi - 2;
+                if (hvezdy[i].Top >= this.Height)
+                {
+                    hvezdy[i].Top = -hvezdy[i].Height;
+                }
+            }
+        }
+
+        private void Neprmun_timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < (neprmun.Length - obtiznost); i++)
+            {
+                if (neprmun[i].Top < this.Height)
+                {
+                    neprmun[i].Visible = true;
+                    neprmun[i].Top += rychlostneprmun;
+                    Zasahneprlodi();
+                }
+                else
+                {
+                    neprmun[i].Visible = false;
+                    int x = rnd.Next(0, 10);
+                    neprmun[i].Location = new Point(neprlode[x].Location.X + 27, neprlode[x].Location.Y + 30);
+                }
+            }
+        }
+
+        private void Neprlode_timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < neprlode.Length; i++)
+            {
+                neprlode[i].Visible = true;
+                neprlode[i].Top += rychlostneprlode;
+
+                if (neprlode[i].Top > this.Height)
+                {
+                    neprlode[i].Location = new Point((i + 1) * 50, -200);
+                }
+            }
+        }
+
+        private void Municehrace_timer_Tick(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < municehrace.Length; i++)
+            {
+                if (municehrace[i].Top > 0)
+                {
+                    municehrace[i].Visible = true;
+                    municehrace[i].Top -= rychlostmunhrace;
+                    Zasahvesmirnoulodi();
+                }
+                else
+                {
+                    municehrace[i].Visible = false;
+                    municehrace[i].Location = new Point(Vesmirnalod.Location.X + 20, Vesmirnalod.Location.Y - i * 30);
+                }
+            }
+        }
+
+        private void Vesmir_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!pauza)
+            {
+                if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+                {
+                    Pohybdoprava_timer.Stop();
+                    Pohybdolu_timer.Stop();
+                    Pohybdoleva_timer.Stop();
+                    Pohybnahoru_timer.Start();
+                }
+                if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+                {
+                    Pohybdoprava_timer.Stop();
+                    Pohybnahoru_timer.Stop();
+                    Pohybdoleva_timer.Stop();
+                    Pohybdolu_timer.Start();
+                }
+                if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+                {
+                    Pohybdoprava_timer.Stop();
+                    Pohybdolu_timer.Stop();
+                    Pohybnahoru_timer.Stop();
+                    Pohybdoleva_timer.Start();
+                }
+                if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+                {
+                    Pohybnahoru_timer.Stop();
+                    Pohybdolu_timer.Stop();
+                    Pohybdoleva_timer.Stop();
+                    Pohybdoprava_timer.Start();
+                }
+            }
+        }
+
+        private void Vesmir_KeyUp(object sender, KeyEventArgs e)
+        {
+            Pohybnahoru_timer.Stop();
+            Pohybdolu_timer.Stop();
+            Pohybdoleva_timer.Stop();
+            Pohybdoprava_timer.Stop();
+            if (e.KeyCode == Keys.P)
+            {
+                pocetzmacknuti++;
+                if (!konechry && !pauza)
+                {
+                    label5.Location = new Point(50, 150);
+                    label5.Text = "PAUZA";
+                    label5.Visible = true;
+                    soundtrack.Play();
+                    StopTimers();
+                    pauza = true;
+                }
+                if(pocetzmacknuti % 2 == 0 && pauza)
+                {
+                    StartTimers();
+                    label5.Visible = false;
+                    soundtrack.Play();
+                    pauza = false;
+
+                }
+            }
+        }
+
+        private void StartTimers()
+        {
+            Background_timer.Start();
+            Neprlode_timer.Start();
+            Municehrace_timer.Start();
+            Neprmun_timer.Start();
+        }
+
+        private void StopTimers()
+        {
+            Background_timer.Stop();
+            Neprlode_timer.Stop();
+            Municehrace_timer.Stop();
+            Neprmun_timer.Stop();
+        }
+
+        private void Pohybdoleva_timer_Tick(object sender, EventArgs e)
+        {
+            if (Vesmirnalod.Left > 10)
+            {
+                Vesmirnalod.Left -= rychlostvesmirnelodi;
+            }
+        }
+
+        private void Pohybdoprava_timer_Tick(object sender, EventArgs e)
+        {
+            if (Vesmirnalod.Right < 580)
+            {
+
+                Vesmirnalod.Left += rychlostvesmirnelodi;
+            }
+        }
+
+        private void Pohybdolu_timer_Tick(object sender, EventArgs e)
+        {
+            if (Vesmirnalod.Top < 380)
+            {
+                Vesmirnalod.Top += rychlostvesmirnelodi;
+
+            }
+        }
+
+        private void Pohybnahoru_timer_Tick(object sender, EventArgs e)
+        {
+            if (Vesmirnalod.Top > 5)
+            {
+                Vesmirnalod.Top -= rychlostvesmirnelodi;
+
+            }
+        }
+
+        private void Zasahneprlodi()
+        {
+            for (int i = 0; i < neprmun.Length; i++)
+            {
+                if (neprmun[i].Bounds.IntersectsWith(Vesmirnalod.Bounds))
+                {
+                    neprmun[i].Visible = false;
+                    Vesmirnalod.Visible = false;
+                    Konechry("KONEC HRY");
+                }
+            }
+        }
+
+        private void Zasahvesmirnoulodi()
+        {
+            for (int i = 0; i < neprlode.Length; i++)
+            {
+                if (municehrace[0].Bounds.IntersectsWith(neprlode[i].Bounds)
+                    || municehrace[1].Bounds.IntersectsWith(neprlode[i].Bounds)
+                    || municehrace[2].Bounds.IntersectsWith(neprlode[i].Bounds))
+                {
+                    skore += 1;
+                    if (skore < 10)
+                    { label3.Text = "0" + skore.ToString(); }
+                    else { label3.Text = skore.ToString(); }
+                    if (skore % 20 == 0)
+                    {
+                        uroven += 1;
+                        if (uroven < 10)
+                        { label1.Text = "0" + uroven.ToString(); }
+                        else { label1.Text = uroven.ToString(); }
+
+                        if (rychlostneprlode <= 11 && rychlostneprmun <= 11 && obtiznost > 0)
+                        {
+                            obtiznost--;
+                            rychlostneprlode++;
+                            rychlostneprmun++;
+                        }
+
+                        if (uroven == 10)
+                        {
+                            Konechry("ZVLÁDLI JSTE \nSKORO NEMOŽNÉ!");
+                        }
+                    }
+                    neprlode[i].Location = new Point((i + 1) * 50, -100);
+                }
+                if (Vesmirnalod.Bounds.IntersectsWith(neprlode[i].Bounds))
+                {
+                    Vesmirnalod.Visible = false;
+                    neprlode[i].Visible = false;
+                    Konechry("KONEC HRY");
+                }
+            }
+        }
+    }
+}
